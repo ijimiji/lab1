25ea2b0c08ed0fc78be9d7915a46986578b9223e
Jahor <jahor@tuta.io>
2020-09-11 10:31:55 +0300

    Add get_taylor_member function. Init vars. Loop.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <cmath>
 
 int factorial(int n)
 {
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
+double get_taylor_member(double x, int i){
+    return pow(x, i) / factorial(i);
+}
+
 int main(){
+    double x;
+    unsigned int k;
+    double result = 0;
+    double epsilon = 0.1;
+
+    for (int i;;i+=2){
+        if (result < epsilon){
+            break;
+        }
+        result += get_taylor_member(x, i);
+    }
+
     return 0;
 }
