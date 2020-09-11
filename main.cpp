bf2f8a263dd69b46715c58003bee1cfc7a356d86
Jahor <jahor@tuta.io>
2020-09-11 12:27:37 +0300

    Add working scratch

--- main.cpp
+++ main.cpp
@@ -1,28 +1,32 @@
-#include <iostream>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
-int factorial(int n)
-{
-  return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-}
+int factorial(int n) { return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n; }
 
-double get_taylor_member(double x, int i){
-    return pow(x, i) / factorial(i);
-}
+// Refactor
+double get_taylor_member(double x, int i) { return pow(x, i) / factorial(i); }
 
-int main(){
+int main() {
     double x;
-    unsigned int k;
+    int k;
+    std::cout << "Enter x ";
+    std::cin >> x;
+    std::cout << "Enter k ";
+    std::cin >> k;
+    double accuracy = pow(10, -k);
     double result = 0;
-    double epsilon = 0.1;
-
-    for (int i;;i+=2){
-        if (result < epsilon){
-            break;
-        }
-        result += get_taylor_member(x, i);
+   
+    for (int i = 1; i; i += 2) {
+        double taylor_member = get_taylor_member(x, i);
+        if (taylor_member > 0 && taylor_member < accuracy) break;
+        result += taylor_member;
     }
-
+   
+    std::cout << std::fixed;
+    std::cout << std::setprecision(16);
+    std::cout << "The result is " << result << std::endl;
+    std::cout << "Result form std is " << sinh(x) << std::endl;
+   
     return 0;
 }
-
