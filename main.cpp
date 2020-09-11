04a0ce217633a5c0e04ad3b26108c24e7d678198
Jahor <jahor@tuta.io>
2020-09-11 17:47:20 +0300

    Remove functions in favour of loops

--- main.cpp
+++ main.cpp
@@ -2,31 +2,27 @@
 #include <iomanip>
 #include <iostream>
 
-int factorial(int n) { return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n; }
-
-// Refactor
-double get_taylor_member(double x, int i) { return pow(x, i) / factorial(i); }
-
 int main() {
     double x;
-    int k;
+    int k; // unsigned int can't be used as negative power in pow
     std::cout << "Enter x ";
     std::cin >> x;
     std::cout << "Enter k ";
     std::cin >> k;
     double accuracy = pow(10, -k);
-    double result = 0;
    
-    for (int i = 1; i; i += 2) {
-        double taylor_member = get_taylor_member(x, i);
-        if (taylor_member > 0 && taylor_member < accuracy) break;
+    double taylor_member = x; // First member is always equals x
+    double result = taylor_member; // First cycle approximation is always x
+    if (!(taylor_member > 0 && taylor_member < accuracy))
+    for (int i = 3; i; i += 2) {
+        taylor_member = taylor_member * x * x / i / (i-1);
+        // Finish calculations if a member is accurate enough
+        if (taylor_member > 0 && taylor_member < accuracy) break; 
         result += taylor_member;
     }
-
     std::cout << std::fixed;
     std::cout << std::setprecision(k);
     std::cout << "The result is " << result << std::endl;
     std::cout << "Result form std is " << sinh(x) << std::endl;
-   
     return 0;
 }
