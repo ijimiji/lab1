d59be11b03fde0b621a437de167a215c59f1dff0
Jahor <jahor@tuta.io>
2020-09-15 20:22:22 +0300

    Add ch(x)

--- main.cpp
+++ main.cpp
@@ -11,15 +11,20 @@ int main() {
     std::cin >> k;
 
     double accuracy = pow(10, -k);
-    double a_n = x, sh = x;
-    int i = 3;
+    double a_n = 1, ch = 1;
+    int i = 2;
     while (abs(a_n *= (x * x / i / (i - 1.0))) > accuracy) {
-        sh += a_n;
+        ch += a_n;
         i += 2;
     }
+
+    double e = std::exp(1.0);
+    double ch_1 = (pow(e, x) + pow(e, -x))/2;
+
     std::cout << std::fixed;
     std::cout << std::setprecision(k);
-    std::cout << "The result is " << sh << std::endl;
-    std::cout << "Result form std is " << sinh(x) << std::endl;
+    std::cout << "The result is " << ch << std::endl;
+    std::cout << "The result from e is " << ch_1 << std::endl;
+    std::cout << "Result form std is " << cosh(x) << std::endl;
     return 0;
 }
