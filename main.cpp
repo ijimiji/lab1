4b9bd618cc610b8d7a146e2d5304f56ca5290f2b
Jahor <jahor@tuta.io>
2020-09-15 19:42:20 +0300

    One line solution

--- main.cpp
+++ main.cpp
@@ -6,23 +6,17 @@ int main() {
     double x;
     std::cout << "Enter x ";
     std::cin >> x;
-    int k; // unsigned int can't be used as negative power in pow
+    int k;
     std::cout << "Enter k ";
     std::cin >> k;
-    double accuracy = pow(10, -k);
 
-    double taylor_member = x; // First member is always equals x
-    double result = x; // First cycle approximation is always x
-    if (!(taylor_member > 0 && taylor_member < accuracy))
-        for (int i = 3; i; i += 2) {
-            taylor_member = taylor_member * x * x / i / (i - 1);
-            // Finish calculations if a member is accurate enough
-            if (taylor_member > 0 && taylor_member < accuracy) break;
-            result += taylor_member;
-        }
+    double accuracy = pow(10, -k);
+    double a_n = x, sh = x;
+    for (int i = 3; abs(a_n *= (x * x / i / (i - 1.0))) > accuracy; i += 2) sh += a_n;
+
     std::cout << std::fixed;
     std::cout << std::setprecision(k);
-    std::cout << "The result is " << result << std::endl;
+    std::cout << "The result is " << sh << std::endl;
     std::cout << "Result form std is " << sinh(x) << std::endl;
     return 0;
 }
