0248b0a03be0688769688c33ba6989f96d08ea3f
Jahor <jahor@tuta.io>
2020-09-17 17:32:19 +0300

    Add round function

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,6 +2,13 @@
 #include <iomanip>
 #include <iostream>
 
+double round(double x, int n) {
+    int d = 0;
+    if ((x * pow(10, n + 1)) - (floor(x * pow(10, n))) > 4) d = 1;
+    x = (floor(x * pow(10, n)) + d) / pow(10, n);
+    return x;
+}
+
 int main() {
     double x;
     std::cout << "Enter x ";
