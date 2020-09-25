56f6d380de3950f9e40247203a1bf1e6e813b198
Jahor <jahor@tuta.io>
2020-09-25 11:40:47 +0300

    Refactor comments

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,15 +3,22 @@
     Variant 10
     No rights preserved
 */
+
 #define N_MAX 20
 #include <iostream>
 #include <cmath>
-#include <random> 
 
 
 using std::cout;
 using std::cin;
 using std::endl;
+
+// Proudly copied from StackOverflow
+double fRand(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
 
 // I don't like to think, so I use templates
 template <typename A>
@@ -32,22 +39,23 @@ int main() {
     int choice, arraySize, isRandom;
     while (1) {
         cout << "Enter size of an array." << endl;
-        cout << "Size must be less than " << N_MAX +1 << ", bigger than 1." << endl;
+        cout << "Size must be less than " << N_MAX + 1 << ", bigger than 1." << endl;
         cin >> arraySize;
 
-        if (arraySize > N_MAX || arraySize < 2){
+        if (arraySize > N_MAX || arraySize < 2) {
             cout << "I can't create such array :0" << endl;
             cout << "I will use the default (" << N_MAX << ")" << endl;
         }
 
-        int* array = new int[arraySize];
+        double* array = new double[arraySize];
 
         cout << "Pass 0 if you want to fill array yourself." << endl;
         cout << "Pass 1 if you want randomly filled array." << endl;
         cin >> isRandom;
 
         switch (isRandom) {
-            // Fill array manually
+            
+        // Fill array manually
         case (0): {
             for (int i = 0; i < arraySize; i++) {
                 cout << "Enter " << i + 1 << "th element: ";
@@ -55,11 +63,11 @@ int main() {
             }
             break;
         }
-                // Fill array with random elements
+                  
+        // Fill array with random elements
         case (1): {
-            std::random_device rd;
-            std::mt19937 mt(rd());
-            int minRand, maxRand;
+            double minRand, maxRand;
+
             cout << "Enter min element for random range." << endl;
             cin >> minRand;
             cout << "Enter max element for random range." << endl;
@@ -69,85 +77,83 @@ int main() {
                 std::swap(minRand, maxRand);
             }
 
-            std::uniform_int_distribution<int> dist(minRand, maxRand);
-
             cout << "Here is array of random elements:" << endl;
             for (int i = 0; i < arraySize; i++) {
-                array[i] = dist(mt);
+                array[i] = fRand(minRand, maxRand);
             }
 
             printArray(arraySize, array);
 
             break;
         }
+
         default: {
             cout << "Something is wrong with your input." << endl;
             return 0;
         }
         }
 
-            // Swap first min and max
-                int min = 0;
-                int max = 0;
-                for (int i = 1; i < arraySize; i++) {
-                    if (std::abs(array[min]) > std::abs(array[i])) {
-                        min = i;
-                    }
-                    if (std::abs(array[max]) < std::abs(array[i])) {
-                        max = i;
-                    }
-                }
-                int t = array[min];
-                array[min] = array[max];
-                array[max] = t;
-                cout << "Here is modified array after swapping:" << endl;
-                printArray(arraySize, array);
-        
-                // Calculate sum of elements after last last minimal element
-            // Find last minimal element
-            min = 0;
-            // Dont search for minimal element is one element array
-                for (int i = 1; i < arraySize; i++) {
-                    if (std::abs(array[min]) >= std::abs(array[i])) {
-                        min = i;
-                    }
-                }
-            int sum = 0;
-            // Calculate sum of elements after min
-            if (min != arraySize - 1) {
-                for (int i = min + 1; i < arraySize; i++) {
-                    sum += array[i];
-                }
-                cout << "Sum of elements after min is " << sum << "." << endl;
+        // Swap first min and max
+        int min = 0;
+        int max = 0;
+        for (int i = 1; i < arraySize; i++) {
+            if (std::abs(array[min]) > std::abs(array[i])) {
+                min = i;
             }
-            else {
-                cout << "Can't calculate sum of elements after min because min is the last element." << endl;
+            if (std::abs(array[max]) < std::abs(array[i])) {
+                max = i;
             }
-        
+        }
+        int t = array[min];
+        array[min] = array[max];
+        array[max] = t;
+        cout << "Here is modified array after swapping:" << endl;
+        printArray(arraySize, array);
 
-                // Replace negative elements with their squares
-                // and print sorted array
+        // Calculate sum of elements after last last minimal element
+        // Find last minimal element
+        min = 0;
+        for (int i = 1; i < arraySize; i++) {
+            if (std::abs(array[min]) >= std::abs(array[i])) {
+                min = i;
+            }
+        }
 
-            // Replace negative elements with their squares
-            for (int i = 0; i < arraySize; i++) {
-                if (array[i] < 0) {
-                    array[i] = array[i] * array[i];
+        // Calculate sum of elements after min
+        int sum = 0;
+        if (min != arraySize - 1) {
+            for (int i = min + 1; i < arraySize; i++) {
+                sum += array[i];
+            }
+            cout << "Sum of elements after min is " << sum << "." << endl;
+        }
+        else {
+            cout << "Can't calculate sum of elements after min because min is the last element." << endl;
+        }
+
+
+        // Replace negative elements with their squares
+        // and print sorted array
+
+        for (int i = 0; i < arraySize; i++) {
+            if (array[i] < 0) {
+                array[i] = array[i] * array[i];
+            }
+        }
+        // Sort with bubble sort
+        for (int i = 0; i < arraySize; i++) {
+
+            for (int j = i + 1; j < arraySize; j++)
+            {
+                if (array[j] < array[i]) {
+                    std::swap(array[i], array[j]);
                 }
             }
-            // Sort with bubble sort
-            for (int i = 0; i < arraySize; i++) {
+        }
+        cout << "Here is sorted array:" << endl;
+        printArray(arraySize, array);
+        break;
 
-                for (int j = i + 1; j < arraySize; j++)
-                {
-                    if (array[j] < array[i]) {
-                        std::swap(array[i], array[j]);
-                    }
-                }
-            }
-            cout << "Here is sorted array:" << endl;
-            printArray(arraySize, array);
-            break;
-        
         // Free memory
         delete[] array;
     }
