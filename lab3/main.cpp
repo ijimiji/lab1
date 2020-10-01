d2e7fccce0cec48950ec0e5bb9d115b30126371c
Jahor <jahor@tuta.io>
2020-10-01 17:22:51 +0300

    Create draft

---
+++ lab3/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void FillMatrix(int** matrix){}
+void TransposeMatrix(int** matrix){}
+void SolveTask1(int** matrix){}
+void SolveTask2(int** matrix){}
+
+int main(){
+    return 0;
+}
