bcd2cf9519d410bf6b0922764bdb4b37de0fad1f
Jahor <jahor@tuta.io>
2020-10-02 13:20:56 +0300

    Improve algorithm for matrix access. Deprecate some memory utilities

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 
-void FreeMemory(int**& matrix, int N) {
+enum Command { RandomlyFillMatrix = 0, ManuallyFillMatrix = 1 };
+
+// Using **& in the functions will make it more memory efficient
+
+void FreeMemory(int **&matrix, int N) {
     for (int i = 0; i < N; ++i) {
         delete[] matrix[i];
     }
 }
 
-void CopyMatrix(int** source, int**& destination, int N) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            destination[i][j] = source[i][j];
-        }
-    }
+int GetMatrixElement(int **&matrix, int i, int j, int N) {
+    // The sum is bigger than N for elements in the second triangle
+    if (i + j >= N) {
+        return matrix[N - 1 - j][N - 1 - i];
+    } else
+        return matrix[i][j];
 }
 
-void AllocateMemory(int**& target, int N) {
-    target = new int*[N];
+void AllocateMemory(int **&target, int N) {
+    target = new int *[N];
     for (int i = 0; i < N; i++) {
-        target[i] = new int[N];
+        target[i] = new int[N - i];
     }
 }
 
@@ -30,38 +34,35 @@ int RandInt(int min, int max) {
     return min + rand() % ((max + 1) - min);
 }
 
-void FillMatrix(int**& matrix, int N) {
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j) {
-            matrix[i][j] = RandInt(-100, 100);
-        }
-}
+void FillMatrixFromRandom(int **&matrix, int N) {
+    int minRand, maxRand;
+    std::cout << "Enter min element for rand range" << std::endl;
+    std::cin >> minRand;
+    std::cout << "Enter max element for rand range" << std::endl;
+    std::cin >> maxRand;
 
-void TransposeMatrix(int**& matrix, int N) {
-    int** matrixCopy;
-    AllocateMemory(matrixCopy, N);
-    CopyMatrix(matrix, matrixCopy, N);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            matrix[i][j] = matrixCopy[N - j - 1][N - i - 1];
+    for (int i = 0; i < N; ++i) {
+        std::cout << "Enter " << N - i << " elements for ";
+        std::cout << i + 1 << "th row" << std::endl;
+        for (int j = 0; j < N - i; ++j) {
+            matrix[i][j] = RandInt(minRand, maxRand);
         }
     }
-    FreeMemory(matrixCopy, N);
 }
 
-void SolveTask1(int** matrix, int N) {
+void SolveTask1(int **&matrix, int N) {
     bool isNegative = false;
     long int product = 1;
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            if (matrix[i][j] < 0) {
+            if (GetMatrixElement(matrix, i, j, N) < 0) {
                 isNegative = true;
                 break;
             }
         }
         if (!isNegative) {
             for (int j = 0; j < N; ++j) {
-                product *= matrix[i][j];
+                product *= GetMatrixElement(matrix, i, j, N);
             }
             std::cout << "Product of the " << i + 1 << "th row is equal ";
             std::cout << product << std::endl;
@@ -71,13 +72,13 @@ void SolveTask1(int** matrix, int N) {
     }
 }
 
-void SolveTask2(int** matrix, int N) {
+void SolveTask2(int **&matrix, int N) {
     int sum = 0;
     int min = 0;
     for (int i = 1; i < N - 1; ++i) {
         sum = 0;
         for (int j = 0; j < N - i; ++j) {
-            sum += matrix[i + j][j];
+            sum += GetMatrixElement(matrix, i + j, j, N);
         }
         if (i == 1 || min > sum) {
             min = sum;
@@ -87,7 +88,7 @@ void SolveTask2(int** matrix, int N) {
     for (int j = 1; j < N - 1; ++j) {
         sum = 0;
         for (int i = 0; i < N - i; ++i) {
-            sum += matrix[i][i + j];
+            sum += GetMatrixElement(matrix, i, i + j, N);
         }
         if (min > sum) {
             min = sum;
@@ -96,32 +97,56 @@ void SolveTask2(int** matrix, int N) {
     std::cout << "Minimal sum of all the diagonals is " << min << std::endl;
 }
 
-void PrintMatrix(int** matrix, int N) {
+void FillMatrixFromKeyboard(int **matrix, int N) {
+    for (int i = 0; i < N; ++i) {
+        std::cout << "Enter " << N - i << " elements for ";
+        std::cout << i + 1 << "th row" << std::endl;
+        for (int j = 0; j < N - i; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+}
+
+void PrintMatrix(int **matrix, int N) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            std::cout << matrix[i][j] << "\t";
+            std::cout << GetMatrixElement(matrix, i, j, N) << "\t";
         }
         std::cout << std::endl;
     }
 }
 
 int main() {
-    int n;
-    int** matrix;
+    int n, commandInt;
+    int **matrix;
 
     do {
-        std::cout << "Enter amount of rows and columns of a matrix"
-                  << std::endl;
+        std::cout << "Enter amount of rows and columns of a matrix";
+        std::cout << std::endl;
         std::cin >> n;
     } while (n < 1 || n > 10);
 
+    std::cout
+        << "Enter 0 to fill matrix randomly. Enter 1 to fill matrix manually"
+        << std::endl;
+    std::cin >> commandInt;
+
     AllocateMemory(matrix, n);
-    FillMatrix(matrix, n);
-    TransposeMatrix(matrix, n);
+
+    switch ((Command)commandInt) {
+
+    case RandomlyFillMatrix:
+        FillMatrixFromRandom(matrix, n);
+        break;
+
+    case ManuallyFillMatrix:
+        FillMatrixFromKeyboard(matrix, n);
+        break;
+    }
+
     PrintMatrix(matrix, n);
     SolveTask1(matrix, n);
     SolveTask2(matrix, n);
     FreeMemory(matrix, n);
     return 0;
 }
-
