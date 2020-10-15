9f972be542c5495d2a545b1d16ced2d376bd024a
Jahor <jahor@tuta.io>
2020-10-15 21:16:32 +0300

    Scan line by line

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,8 +1,18 @@
 //  Var 10
 #include <fstream>
 #include <iostream>
+#include <utility>
 #include <vector>
 #define N 300
+
+int strLen(char *string) {
+    int n = 0;
+    while (*string) {
+        ++n;
+        ++string;
+    }
+    return n;
+}
 
 int _strspn(const char *string, const char *strCharSet) {
     int n;
@@ -59,35 +69,6 @@ int countChar(const char *string, char target) {
     return i;
 }
 
-int countWordsInLine(char *string) {
-    int n = 0;
-    bool last_is_space = true;
-    while (*string) {
-        if (*string != ' ') {
-            if (last_is_space)
-                ++n;
-            last_is_space = false;
-        } else {
-            last_is_space = true;
-        }
-        ++string;
-    }
-    return n;
-}
-
-std::vector<int> countWordsInFile(const char *file) {
-    std::vector<int> arr;
-    char line[N];
-    std::fstream fin(file);
-    if (fin.is_open()) {
-        while (fin.getline(line, N)) {
-            arr.push_back(countWordsInLine(line));
-        }
-    }
-    fin.close();
-    return arr;
-}
-
 void findWordWithMaxZeros(char *word, int &n, int &n_max, char *last,
                           char *last_but_one) {
     if (strIsMadeOfDigits(word)) {
@@ -104,6 +85,43 @@ void findWordWithMaxZeros(char *word, int &n, int &n_max, char *last,
     }
 }
 
+void wordFromIndex(char *source, char *dest, std::pair<int, int> start_end) {
+    int start = std::get<0>(start_end);
+    int end = std::get<1>(start_end);
+
+    int i, j;
+    for (i = start, j = 0; i <= end; ++i, ++j) {
+        dest[j] = source[i];
+    }
+    dest[j] = '\0';
+}
+
+std::vector<std::pair<int, int>> cutLine(char *line) {
+    bool last_is_space = true;
+    int start = 0;
+    int end = 0;
+    int n = strLen(line);
+    std::vector<std::pair<int, int>> wordInds;
+
+    for (int i = 0; i < n; ++i) {
+        if (line[i] != ' ' && last_is_space) {
+            start = i;
+            last_is_space = false;
+        }
+        if (line[i] == ' ' && !last_is_space) {
+            end = i - 1;
+            wordInds.push_back(std::make_pair(start, end));
+            last_is_space = true;
+        }
+        if (i == n - 1 && start > end) {
+            end = i;
+            wordInds.push_back(std::make_pair(start, end));
+        }
+    }
+
+    return wordInds;
+}
+
 int main() {
     char word[N];
     char subWord[N];
@@ -111,26 +129,27 @@ int main() {
     char last[N];
     char last_but_one[N];
     int n = 0, n_max = 0;
-
-    std::vector<int> amountOfWordsInFile;
+    std::vector<std::pair<int, int>> wordInds;
 
     std::fstream input_fileA("input_a", std::fstream::in);
     std::fstream input_fileB("input_b", std::fstream::in);
 
     // Task A
-    amountOfWordsInFile = countWordsInFile("input_a");
-    for (auto amountOfWordsInLine : amountOfWordsInFile) {
-        input_fileA >> word;
-        input_fileA >> subWord;
+
+    while (input_fileA.getline(line, N)) {
+        wordInds = cutLine(line);
+        wordFromIndex(line, word, wordInds[0]);
+        wordFromIndex(line, subWord, wordInds[1]);
         std::cout << _strspn(word, subWord) << std::endl;
     }
+
     input_fileA.close();
 
     // Task B
-    amountOfWordsInFile = countWordsInFile("input_b");
-    for (auto wN : amountOfWordsInFile) {
-        for (int i = 0; i < wN; ++i) {
-            input_fileB >> word;
+    while (input_fileB.getline(line, N)) {
+        wordInds = cutLine(line);
+        for (std::pair<int, int> wordInd : wordInds) {
+            wordFromIndex(line, word, wordInd);
             findWordWithMaxZeros(word, n, n_max, last, last_but_one);
         }
         if (n_max == 0) {
@@ -141,5 +160,6 @@ int main() {
         n = 0;
         n_max = 0;
     }
+
     input_fileB.close();
 }
